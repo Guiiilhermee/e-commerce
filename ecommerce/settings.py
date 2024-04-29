4964296271530460e110884b77dc695ece31c9e0
Guiiilhermee rodrigues <guilherme_rodrigues91@hotmail.com>
2024-04-29 18:21:03 +0000

    Added stripe and basic functionality

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -180,3 +180,11 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Stripe
+# Stripe
+FREE_DELIVERY_THRESHOLD = 50
+STANDARD_DELIVERY_PERCENTAGE = 10
+STRIPE_CURRENCY = 'EUR'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
